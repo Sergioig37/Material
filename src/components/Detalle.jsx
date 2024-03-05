fdfdf2e6645c15a38263474dc010ae1932b51718
Sergioig37 <sergioig37@educastur.es>
2024-03-05 10:12:33 +0100

    form box

--- src/components/Detalle.jsx
+++ src/components/Detalle.jsx
@@ -2,7 +2,10 @@ import { useNavigate, useParams } from "react-router-dom";
 import * as React from "react";
 import Button from "@mui/material/Button";
 import { useState, useEffect } from "react";
-import Stack from "@mui/material/Stack";
+
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Avatar } from "@mui/material";
 
 export const Detalle = () => {
   const { id } = useParams();
@@ -23,7 +26,7 @@ export const Detalle = () => {
       .then((res) => res.json())
       .then((result) => {
         setNombre(result.user.fname);
-        setApellido(result-user.lname);
+        setApellido(result.user.lname);
         setUsername(result.user.username);
         setEmail(result.user.email);
         setAvatar(result.user.avatar);
@@ -33,9 +36,51 @@ export const Detalle = () => {
 
   return (
     <>
+
+<Avatar src={avatar}></Avatar>
+
+<Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '40ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
       <div>
-        Detalle {id}
-        </div>
+        <TextField
+          required
+          id="outlined-required"
+          label="Nombre"
+          defaultValue="Hello World"
+          value={nombre}
+        />
+        <TextField
+          disabled
+          id="outlined-disabled"
+          label="Apellido"
+          value={apellido}
+        />
+        <TextField
+          id="outlined-read-only-input"
+          label="Email"
+          defaultValue="Hello World"
+          value={email}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <TextField
+          id="outlined-read-only-input"
+          label="Username"
+          defaultValue="Hello World"
+          value={username}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+      </div>  
+    </Box>
       <Button onClick={handleAtras} size="small" variant="outlined">Atrás</Button>
     </>
   );
